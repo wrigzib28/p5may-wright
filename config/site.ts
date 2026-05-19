3af6f8753e095b8207ba188e44052857a8e3ebff
wrigzib28 <zdw2028@gmail.com>
2026-05-19 13:14:37 -0700

    Add files via upload

---
+++ config/site.ts
@@ -0,0 +1,56 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: "Discount Time Machine Wholesale",
+  description:
+    "A great place to buy time machines at ridiculous prices! Obviously this is example text. Write a description of what your site is / what it does.",
+  navItems: [
+    {
+      label: "Home",
+      href: "/",
+    },
+    {
+      label: "Docs",
+      href: "/docs",
+    },
+    {
+      label: "Pricing",
+      href: "/pricing",
+    },
+    {
+      label: "Blog",
+      href: "/blog",
+    },
+    {
+      label: "About",
+      href: "/about",
+    },
+  ],
+  navMenuItems: [
+    {
+      label: "Home",
+      href: "/",
+    },
+    {
+      label: "Docs",
+      href: "/docs",
+    },
+    {
+      label: "Pricing",
+      href: "/pricing",
+    },
+    {
+      label: "Blog",
+      href: "/blog",
+    },
+    {
+      label: "About",
+      href: "/about",
+    },
+  ],
+  links: {
+    instagram: "#",
+    facebook: "#",
+    twitter: "#",
+  },
+};
